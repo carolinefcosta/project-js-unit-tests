8f1236ac543c269a965ea7aa57e5ccc4357fe146
Caroline <carolinefernandesc@hotmail.com>
2022-11-24 08:01:16 -0300

    corrigindo lint

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  let objeto = {
-    name: nome,
-    feedback: 
-  }
-};
+const createStudent = (nome) => {};
 
 module.exports = createStudent;
